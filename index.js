0a547b6d32add130e46f97b37065dffde8034169
agesam <152259135+agesam@users.noreply.github.com>
2025-09-18 15:57:57 +0800

    index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ export default {
         }
 
         if (request.method !== 'POST') {
-            return new Response('NOTHING HERE', { status: 405 });
+            return new Response(apiKey, { status: 405 });
         }
 
         try {
@@ -68,3 +68,4 @@ export default {
         }
     },
 };
+
