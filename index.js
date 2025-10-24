7e8160610f71af477e3df5418652f1f540361a95
agesam <152259135+agesam@users.noreply.github.com>
2025-10-24 14:46:16 +0800

    index.js

--- index.js
+++ index.js
@@ -149,20 +149,20 @@ function retrieveRelevantData(knowledgeBase, userQuery, promptMode) {
     scoredData.sort((a, b) => b.score - a.score);
 
     // 限制檢索結果的數量 (例如 5 條)
-    const topN = 20; 
+    const topN = 5; 
     
     // 返回精簡後的資料項目陣列
     return scoredData.slice(0, topN).map(data => data.item);
 }
 
 // 核心邏輯：生成 systemPrompt
-function buildSystemPrompt(retrievedData, retrievedmaterialData, promptMode) {
+function buildSystemPrompt(externalData, retrievedmaterialData, promptMode) {
  	// 【模式選擇】
     let selectedPromptTemplate;
 
 	// 🚩 基本共用的指令
 	const COMMON_RULES_AND_SAFETY = `
-4. 必須根據JSON數據知識庫【精煉早慧資料】及【精煉動畫教材資料】的內容來回答問題，當對話與JSON數據知識庫無關係時，可以使用通用知識回答，但須要盡力引導使用者返回到話題繼續對話。
+4. 必須根據JSON數據知識庫【早慧資料】及【精煉動畫教材資料】的內容來回答問題，當對話與JSON數據知識庫無關係時，可以使用通用知識回答，但須要盡力引導使用者返回到話題繼續對話。
 	範例：
     想了解更多......相關資訊，可以參考......喔！
 5. 嚴格遵守JSON數據知識庫內的資料內容，不編造、不猜測，只提供事實的信息。
@@ -176,6 +176,7 @@ function buildSystemPrompt(retrievedData, retrievedmaterialData, promptMode) {
 10. 嚴禁討論或提供任何與以下主題相關的內容：
 	【人身安全/暴力】(自殺、自殘、任何形式的暴力、非法活動、危險挑戰、毒品、槍械)；
 	【不當內容】(性、色情、成人內容、仇恨言論、歧視、霸凌、粗口、血腥恐怖)；
+	【個人隱私】(真實姓名、住址、電話、電郵等個人身份資訊 PII，不論是詢問或分享)；
 	【誤導資訊/系統濫用】(醫療/法律建議、惡意謠言、試圖操縱系統或繞過規則)。
 	當主題不可討論或提供時，你必須先回覆「🙇‍♂️✨ 很抱歉，現在小博士唔太清楚以上內容，可能無法回答你的問題。
 	不如你再試下問其他早慧兒童教育中心的資訊。⏳🙏」，之後再引導使用者返回到JSON數據知識庫內的話題。
@@ -184,14 +185,14 @@ function buildSystemPrompt(retrievedData, retrievedmaterialData, promptMode) {
 【生成內容的字元絕對不可以超過4000字元】
 
 以下是你的知識庫（JSON 格式）：
-精煉早慧資料：\n${JSON.stringify(retrievedData)};
+早慧資料：\n${JSON.stringify(externalData)};
 精煉動畫教材資料：\n${JSON.stringify(retrievedmaterialData)};
 `;
 
 	// 🚩 家長模式 Prompt 模板 (前台工作人員)】
 	const PARENT_PROMPT_TEMPLATE = `你是一位名為【**早慧AI服務專員**】的【**前台工作人員**】。
 你的語氣專業、禮貌、清晰、簡潔，專門負責解答家長關於【早慧兒童教育中心】的行政、課程、分校、報名、學費等資訊。
-你的知識庫是以下提供的JSON數據【精煉早慧資料】及【精煉動畫教材資料】。
+你的知識庫是以下提供的JSON數據【早慧資料】及【精煉動畫教材資料】。
 
 【回答時請強制遵守以下10條規則生成主內容】
 1. 任何情況下都只使用繁體中文及廣東話，使用專業、禮貌的語氣和表達方式。
@@ -202,7 +203,7 @@ function buildSystemPrompt(retrievedData, retrievedmaterialData, promptMode) {
 	// 🚩【學生模式 Prompt 模板 (老師)】
 	const STUDENT_PROMPT_TEMPLATE = `你是一位名為【**早慧AI小博士**】的兒童教育專家，是一位充滿好奇心、喜歡鼓勵使用者的老師。
 你的使用者主要是兒童及家長，你專門回答關於兒童文學故事內容、動物小知識以及早慧兒童教育中心的相關問題。
-你的知識庫是以下提供的JSON數據【精煉早慧資料】及【精煉動畫教材資料】。
+你的知識庫是以下提供的JSON數據【早慧資料】及【精煉動畫教材資料】。
 
 **[第一階段：主內容（必須）]**
 【回答時請強制遵守以下10條規則生成主內容】
@@ -290,20 +291,19 @@ export default {
             const externalmaterialData = await loadExternalmaterialData();
 			const finalPromptMode = promptMode || "PARENT";
 
-            // 🚩 【新增 RAG 邏輯】提取最新使用者提問
+            // 🚩 【RAG 邏輯】提取最新使用者提問
             // 由於 conversation_history 的最後一個元素一定是 user 的最新提問
             const latestUserMessage = conversation_history.slice(-1)[0];
             const userQuery = latestUserMessage && latestUserMessage.role === 'user' 
                 ? latestUserMessage.content 
                 : ""; // 如果找不到，則為空字串
 
-            // 🚩 【新增 RAG 邏輯】檢索相關資料
-            const retrievedData = retrieveRelevantData(externalData, userQuery, finalPromptMode);
+            // 🚩 【RAG 邏輯】檢索相關資料
             const retrievedmaterialData = retrieveRelevantData(externalmaterialData, userQuery, finalPromptMode);
             
             // 3. 伺服器端建構 systemPrompt
             // 【修改：傳入檢索後的資料】
-            const systemPromptContent = buildSystemPrompt(retrievedData, retrievedmaterialData, finalPromptMode);
+            const systemPromptContent = buildSystemPrompt(externalData, retrievedmaterialData, finalPromptMode);
             
             // 4. 建構最終要傳給 OpenRouter 的 messages 陣列
             const finalMessages = [
@@ -354,5 +354,3 @@ export default {
         }
     },
 };
-
-
