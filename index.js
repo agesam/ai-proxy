f0ed5c3ef18f86861d75140b371d1c06c1680e8e
agesam <152259135+agesam@users.noreply.github.com>
2025-11-03 15:39:23 +0800

    index.js

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ B
 
 export default {
     async fetch(request) {
-        const apiKey = Deno.env.get("OPENROUTER_API_KEY");
+        const apiKey = Deno.env.get("OPENROUTER_API_KEY_BACKUP");
         if (!apiKey) {
             return new Response("Missing OPENROUTER_API_KEY", { status: 500 });
         }
@@ -357,3 +357,4 @@ export default {
         }
     },
 };
+
